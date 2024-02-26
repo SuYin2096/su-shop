1f286291b80ab82e5a6e3566c5adf97c699f32c9
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-02-26 14:26:37 +0630

    initial commit for inventory-service

---
+++ inventory-service/src/test/java/com/scarlettech/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarlettech.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
