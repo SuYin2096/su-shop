ab4835b67a3a583bd16ba9b9e73c681bdb18c728
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-02-26 10:48:37 +0630

    do code refactoring

--- product-service/src/main/java/com/scarlettech/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/scarlettech/productservice/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.scarlettech.productservice.repository;
 
 import com.scarlettech.productservice.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
 }
