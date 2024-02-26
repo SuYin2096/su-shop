0151ca3ed8f890d50e59aa9dfa2eaee4c4303eb9
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-02-26 17:00:58 +0630

    add web client config

--- order-service/src/main/java/com/scarlettech/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/scarlettech/orderservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.scarlettech.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +19,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest){
        Order order = new Order();
@@ -29,7 +31,20 @@ public class OrderService {
                .toList();
 
        order.setOrderLineItemsList(orderLineItems);
-       orderRepository.save(order);
+
+        // call inventory service and place order if product is in the stock
+        Boolean result = webClient.get()
+                .uri("http://localhost:8083/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+
+        if(result){
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is not in stock, please try again later.");
+        }
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsRequest orderLineItemsRequest){
