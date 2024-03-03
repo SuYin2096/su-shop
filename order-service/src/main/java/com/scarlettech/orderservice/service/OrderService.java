d1ccc9e5fdc4213b693097a29425d23a5f59fb0d
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-03-03 19:05:42 +0630

    update web client of order service

--- order-service/src/main/java/com/scarlettech/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/scarlettech/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
        Order order = new Order();
@@ -37,7 +37,7 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
         // call inventory service and place order if product is in the stock
-        InventoryResponse[] inventoryResponses = webClient.get()
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
                 .uri("http://invertory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
