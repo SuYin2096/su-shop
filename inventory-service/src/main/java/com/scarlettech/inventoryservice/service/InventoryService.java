d91b8145dd16cce2d64ef27ce94c0fdf68bf1b2b
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-03-12 10:34:39 +0630

    add circuit breaker and time limiter

--- inventory-service/src/main/java/com/scarlettech/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/scarlettech/inventoryservice/service/InventoryService.java
@@ -4,6 +4,8 @@ import com.scarlettech.inventoryservice.dto.InventoryResponse;
 import com.scarlettech.inventoryservice.model.Inventory;
 import com.scarlettech.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,12 +16,17 @@ import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInstock(List<String> skuCode){
+        log.info("Wait Started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder()
